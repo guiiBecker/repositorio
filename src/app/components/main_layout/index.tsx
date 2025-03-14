da2e5f68defac7133d4cc772161933c4ca5d8e21
guiiBecker <guilhermegbecker@gmail.com>
2025-03-14 14:46:31 -0300

    seloko

---
+++ src/app/components/main_layout/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+
+export const  MainContainer = styled.div
+`
+ margin: 0px;
+ padding: 0px;
+`;
+
+
+
+
+
+
+
+export{}
---
+++ src/app/components/main_layout/styles.ts
@@ -0,0 +1,23 @@
+// import Header from "../header";
+// import { MainContainer,  } from "./style";
+
+
+
+// interface Props{
+//     children: React.ReactNode;
+// }
+// export default function MainLayout({children}: Props) {
+
+    
+
+//     return(
+//             <MainContainer>
+//                 <Header></Header>
+//                 {children}
+//             </MainContainer>  
+//     );
+
+// };
+
+
+// export{}
